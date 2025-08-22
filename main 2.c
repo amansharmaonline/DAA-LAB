8769a7b7b7702132f88b2fe7a8b4d06a4bdfd16f
amansharmaonline <76041215+amansharmaonline@users.noreply.github.com>
2025-08-22 11:56:48 +0530

    Add files via upload

---
+++ main 2.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DAA 1
+//
+//  Created by Aman on 22/08/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
