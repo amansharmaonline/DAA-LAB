8bcf85f203e366ceeb396976a71ecc145b18554e
amansharmaonline <76041215+amansharmaonline@users.noreply.github.com>
2025-12-14 18:47:23 +0530

    Update LLCS.cpp

--- Week 9/LLCS.cpp
+++ Week 9/LLCS.cpp
@@ -1,97 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-// direction codes: 1=diag(match), 2=tie(up+left), 3=up, 4=left
-static void dfsAllLCS(int i, int j,
-                      const string& s1,
-                      const vector<vector<int>>& dir,
-                      string& cur,               // building reversed
-                      vector<string>& out)       // "normal array" of strings
-{
-    if (i==0 || j==0) 
-    {
-        string t = cur;
-        reverse(t.begin(), t.end());
-        out.push_back(t);
-        return;
+int main() {
+    char s1[100], s2[100];
+    int dp[101][101];
+    int m, n, i, j;
+
+    printf("Enter first string: ");
+    scanf("%s", s1);
+
+    printf("Enter second string: ");
+    scanf("%s", s2);
+
+    m = strlen(s1);
+    n = strlen(s2);
+
+    // Step 1: initialize first row and column to 0
+    for (i = 0; i <= m; i++) {
+        dp[i][0] = 0;
     }
-    int d = dir[i][j];
-    if (d == 1) // go diagonal and append char
-    {                   
-        cur.push_back(s1[i-1]);
-        dfsAllLCS(i-1, j-1, s1, dir, cur, out);
-        cur.pop_back();
-    } 
-    else 
-    {
-        if (d == 3 || d == 2) // up
-        {     
-            dfsAllLCS(i-1, j, s1, dir, cur, out);
-        }
-        if (d == 4 || d == 2) // left
-        {     
-            dfsAllLCS(i, j-1, s1, dir, cur, out);
-        }
+    for (j = 0; j <= n; j++) {
+        dp[0][j] = 0;
     }
-}
 
-int lcs(const string &s1, const string &s2,vector<vector<int>>& dir)
-{
-    int m = s1.size(), n = s2.size();
-    vector<vector<int>> len(m+1, vector<int>(n+1, 0));
-    dir.assign(m+1, vector<int>(n+1, 0));
+    // Step 2: fill dp table (same as before)
+    for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) {
 
-    for (int i=1;i<=m;++i){
-        for (int j=1;j<=n;++j){
-            if (s1[i-1]==s2[j-1]) 
-            {
-                len[i][j] = len[i-1][j-1] + 1;
-                dir[i][j] = 1;            // diag
-            } 
-            else 
-            {
-                if (len[i-1][j] > len[i][j-1]) 
-                {
-                    len[i][j] = len[i-1][j];
-                    dir[i][j] = 3;        // up
-                } 
-                else if (len[i-1][j] < len[i][j-1]) 
-                {
-                    len[i][j] = len[i][j-1];
-                    dir[i][j] = 4;        // left
-                } 
-                else 
-                {
-                    len[i][j] = len[i-1][j]; // tie
-                    dir[i][j] = 2;        // tie -> branch
+            if (s1[i-1] == s2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];  // characters match
+            }
+            else {
+                if (dp[i-1][j] > dp[i][j-1]) {
+                    dp[i][j] = dp[i-1][j];     // choose top
+                } else {
+                    dp[i][j] = dp[i][j-1];     // choose left
                 }
             }
         }
     }
-    return len[m][n];
-}
 
-int main() {
-    string s1 = "ABC";
-    string s2 = "CBA";
+    printf("Length of LCS = %d\n", dp[m][n]);
 
-    vector<vector<int>> direction;
-    int L = lcs(s1, s2, direction);
+    // Step 3: find the actual LCS by backtracking
+    int index = dp[m][n];      // length of LCS
+    char lcs[101];
+    lcs[index] = '\0';         // null terminate
 
-    vector<string> all_lcs;
-    string cur;
-    dfsAllLCS((int)s1.size(), (int)s2.size(), s1, direction, cur, all_lcs);
+    i = m;
+    j = n;
 
-    // de-dup and present cleanly
-    sort(all_lcs.begin(), all_lcs.end());
-    all_lcs.erase(unique(all_lcs.begin(), all_lcs.end()), all_lcs.end());
+    // Start from dp[m][n] and move backwards
+    while (i > 0 && j > 0) {
 
-    cout << "LCS length: " << L << "\n";
-    cout << "All LCS (" << all_lcs.size() << "):\n";
-    for (int i=0;i<all_lcs.size();i++)
-    {
-        cout << all_lcs[i] << "\n";
-    } 
+        // If characters match, they are part of the LCS
+        if (s1[i-1] == s2[j-1]) {
+            lcs[index - 1] = s1[i - 1]; // put character into LCS
+            i--;
+            j--;
+            index--;
+        }
+        else {
+            // Move in direction of larger dp value
+            if (dp[i-1][j] > dp[i][j-1]) {
+                i--;    // move up
+            } else {
+                j--;    // move left
+            }
+        }
+    }
+
+    printf("LCS = %s\n", lcs);
+
+    return 0;
 }
