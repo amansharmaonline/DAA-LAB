379fd046ca53a969deca0e0e6e9aeea20d1bf473
amansharmaonline <76041215+amansharmaonline@users.noreply.github.com>
2025-12-14 18:51:54 +0530

    Update and rename RabinKarp.c to Rabin_Karp.c

--- Week 10/Rabin_Karp.c
+++ Week 10/Rabin_Karp.c
@@ -4,46 +4,52 @@
 #define d 256
 #define q 13
 
-int main() 
+int main()
 {
-    char text[] = "ABCCDDAEFG";
-    char pat[]  = "CDD";
+    char text[100];
+    char pat[100];
+
+    printf("Enter the text: ");
+    scanf("%s", text);
+
+    printf("Enter the pattern to search: ");
+    scanf("%s", pat);
 
     int n = strlen(text);
     int m = strlen(pat);
 
     int p = 0, t = 0, h = 1;
     
-    // compute h = d^(m-1) % q
+    // compute hash function
     for (int i = 0; i < m - 1; i++)
         h = (h * d) % q;
 
-    // compute initial hashes for pattern and first window
-    for (int i = 0; i < m; i++) 
+    // pattern, block 1
+    for (int i = 0; i < m; i++)
     {
         p = (d * p + pat[i]) % q;
         t = (d * t + text[i]) % q;
     }
 
-    // slide the window
-    for (int i = 0; i <= n - m; i++) 
+    // iterate blocks
+    for (int i = 0; i <= n - m; i++)
     {
-        if (p == t) 
+        if (p == t)
         {
             int match = 1;
-            for (int j = 0; j < m; j++) 
+            for (int j = 0; j < m; j++)
             {
                 if (text[i + j] != pat[j])
-                 match = 0;
+                  match = 0;
             }
             if (match)
                 printf("Match at index %d\n", i);
         }
 
-        if (i < n - m) // within bounds
+        if (i < n - m) 
         {
-            t = (d * (t - text[i] * h)  + text[i + m]) % q; // (d * (t - old * h) + new) % q
-            if (t < 0) 
+            t = (d * (t - text[i] * h)  + text[i + m]) % q;
+            if (t < 0)
                 t += q;
         }
     }
